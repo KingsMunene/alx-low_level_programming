9744fd88efb8e18108b43b3f18637167b98d111e
KingsMunene <munenekings2@gmail.com>
2022-03-19 20:20:06 +0000

    This c file outputs a phrase using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Main of the program aka Entry point
+ *
+ */
+
+int main(void)
+{
+	/* Use put to output a phrase */
+	puts("Programming is like building a multilingual puzzle \n");
+	return (0);
+}

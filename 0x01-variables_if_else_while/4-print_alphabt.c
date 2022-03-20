e946f43788c0ad8d33e44340e190978156b47157
KingsMunene <munenekings2@gmail.com>
2022-03-20 11:20:17 +0000

    Prints alphabets in lower case except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char start = 'a';
+
+for (a = start; a <= 'z'; a++)
+if (a != e || a != q)
+putchar (a);
+
+return (0);
+
+}

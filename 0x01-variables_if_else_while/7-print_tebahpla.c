bd893ffbb11493bb2795f9a7e8411e0131267ed1
KingsMunene <munenekings2@gmail.com>
2022-03-20 12:30:50 +0000

    lowercase in reverse code

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char top = 'z';
+while (top > 'a')
+putchar(top);
+top--;
+
+putchar('\n');
+return (0);
+}

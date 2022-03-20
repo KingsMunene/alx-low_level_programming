e0ba57c36d989bdf7119fe7771be6720f2517ae1
KingsMunene <munenekings2@gmail.com>
2022-03-20 14:05:22 +0000

    digit combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,15 @@ int main(void)
 int number;
 
 for (number = 0; number < 10; number++)
+{
 putchar (number + '0');
 
-if (number == 10)
+if (number < 10)
 {
-break;
-}
 putchar (',');
 putchar (' ');
-
+}
+}
 putchar ('\n');
 return (0);
 

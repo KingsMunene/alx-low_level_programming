fa4e4d7d7958ed5e68fb94ee0c3fa705c6dddadf
KingsMunene <munenekings2@gmail.com>
2022-03-20 01:05:49 +0000

    Checks the last number of a random value

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,18 +11,18 @@
 
 int main(void)
 {
-int n,number;
+int n, number;
 
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-number = n%10;
+number = n % 10;
 
-if(number > 5 )
+if (number > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, number);
 else if (number == 0)
-printf("Last digit of %d is %d and is zero\n", n, number);
+printf("Last digit of %d is %d and is 0\n", n, number);
 else
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, number);
 

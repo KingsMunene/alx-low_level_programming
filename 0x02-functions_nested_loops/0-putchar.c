4a07a58efd651168cb860f18f8315178ec859418
KingsMunene <munenekings2@gmail.com>
2022-03-21 15:30:47 +0000

    printf code file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <main.h>
 /**
  * main - Entry point of the program
  *
@@ -7,6 +8,6 @@
 
 int main(void)
 {
-printf("_putchar\n");
+_putchar("_putchar\n");
 return (0);
 }

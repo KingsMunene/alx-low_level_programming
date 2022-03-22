c8027e1e90639093331fe86389c25b7486dd4220
KingsMunene <munenekings2@gmail.com>
2022-03-22 17:10:15 +0000

    prints n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,37 +7,11 @@
  * print_to_98 - this function prints natural numbers from n to 98
  * @n: is the input
  */
-
 void print_to_98(int n)
 {
 if (n > 98)
 {
-
 while (n >= 98)
-{
-
-if(n != 98)
-{
-
-printf("%d, ", n);
-
-}
-
-else
-{
-
-printf("%d ", n);
-
-}
-
-n--;
-
-}
-
-}
-else
-{
-while(n <= 98)
 {
 if (n != 98)
 {
@@ -45,11 +19,25 @@ printf("%d, ", n);
 }
 else
 {
-printf("%d", n);
+printf("%d ", n);
+}
+n--;
+}
+}
+else
+{
+while (n <= 98)
+{
+if (n != 98)
+{
+printf("%d, ", n);
+}
+else
+{
+printf("%d ", n);
 }
 n++;
 }
-
 printf("\n");
 }
 }

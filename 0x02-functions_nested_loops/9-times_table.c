c9560a2b65127c8e8168f584e86644bb43ebf594
KingsMunene <munenekings2@gmail.com>
2022-03-22 14:40:02 +0000

    multiples of 9 are pretented

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table - this function prints 9 times table starting from 0
+ */
+
+void times_table(void)
+{
+int main,mult,ans;
+
+for (main = 0; main <= 9; main++)
+{
+for (mult = 0; mult <= 9; mult++)
+{
+ans = main * mutl;
+if (mult != 9)
+{
+_putchar (ans + '0');
+_putchar (',');
+}
+else
+{
+_putchar (ans);
+}
+}
+_putchar ('\n');
+}
+}

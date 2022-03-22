bbc962e4ac97fdd67bc792d5a105368b69b8305d
KingsMunene <munenekings2@gmail.com>
2022-03-22 12:35:25 +0000

    code returns the absolute of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main -
+ * @n: 
+ * Return: returns the absolute form of a number
+ */
+
+int _abs(int a)
+{
+if (a < 0)
+{
+return (a * -1);
+}
+else
+{
+return (a);
+}
+}

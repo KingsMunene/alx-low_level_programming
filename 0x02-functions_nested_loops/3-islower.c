b2ed303f25224780c9a0c87420fc591fba29a411
KingsMunene <munenekings2@gmail.com>
2022-03-22 09:26:39 +0000

    checks if a letter is lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * _islower: checks if an alphabet is in lower case
+ * int c - is the input being checked
  */
 
 int _islower(int c)
 {
-if (c < 97)
+if (c < 'a')
 {
 return (0);
 }

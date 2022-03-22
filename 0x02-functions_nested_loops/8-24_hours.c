556445e4e68aa90dc15f7d9420124feb6c97aa75
KingsMunene <munenekings2@gmail.com>
2022-03-22 14:28:36 +0000

    timer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,10 +12,10 @@ for (hour = 0; hour < 24; hour++)
 {
 for (minutes = 0; minutes < 60; minutes++)
 {
-_putchar ((hour \ 10) + '0');
-_putchar ((hour % 10) + '0;);
+_putchar ((hour / 10) + '0');
+_putchar ((hour % 10) + '0');
 _putchar (':');
-_putchar ((minutes \ 10) + '0');
+_putchar ((minutes / 10) + '0');
 _putchar ((minutes % 10) + '0');
 _putchar ('\n');
 }

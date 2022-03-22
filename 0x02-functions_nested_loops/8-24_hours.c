1149f442d307af070338edcc185879be092cfe7a
KingsMunene <munenekings2@gmail.com>
2022-03-22 13:54:48 +0000

    timer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ */
+
+void jack_bauber(void)
+{
+int hour, minutes;
+
+for (hour = 0; hour <= 24; hour++)
+{
+for (minutes = 0; minutes <= 60; minutes++)
+{
+if (hour < 10)
+{
+_putchar (0 + '0');
+_putchar (hour + '0');
+
+}
+else
+{
+_putchar (hour + '0');
+}
+
+_putchar (':');
+
+if (minutes < 10)
+{
+_putchar (0 + '0');
+_putchar (minutes + '0');
+}
+else
+{
+_putchar (minutes + '0');
+}
+
+}
+}
+}

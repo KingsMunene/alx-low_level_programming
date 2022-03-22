66ece45d6422d3250b7df327dcb555c99a691f8e
KingsMunene <munenekings2@gmail.com>
2022-03-22 09:32:36 +0000

    checks if a letter is lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * _islower checks if an alphabet is in lower case
- * c - is the input being checked
- */
+ *_islower checks if an alphabet is in lower case
+ *parameter 'c' - is the input being checked
+ *return: returns 0 if not lower and 1 if
+ * /
 
 int _islower(int c)
 {

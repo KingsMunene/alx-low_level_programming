c516abcc299ea55fc9123ab60082f943f79ee4ca
KingsMunene <munenekings2@gmail.com>
2022-03-22 08:38:01 +0000

    Alphabets printing 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabets in lower case 10 times
+ */
+
+
+
+void print_alphabet_x10(void)
+{
+int i = 0;
+
+while(i <= 10)
+{
+for (j = 'a'; j < 'z'; j++)
+{
+_putchar (j);
+}
+_putchar ('\n');
+}
+}

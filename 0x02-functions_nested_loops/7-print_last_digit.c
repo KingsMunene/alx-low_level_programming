840d504ff29a68f192f8be2d518e11c9218ca545
KingsMunene <munenekings2@gmail.com>
2022-03-22 13:00:00 +0000

    This code returns the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - Checks the last digit in a number
+ * @a: The number to be checked
+ * Return: Returns the last digit
+ */
+
+print_last_digit(int a)
+{
+return (n % 10);
+}

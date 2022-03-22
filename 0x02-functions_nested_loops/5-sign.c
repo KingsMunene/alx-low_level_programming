6b9df0a4395a1291cb024d51f8187be58c30d955
KingsMunene <munenekings2@gmail.com>
2022-03-22 12:05:02 +0000

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - checks if input is greater than 1, if it is 0, or less than 0
+ * Return: returns 1,0 or -1
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar ('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar (0);
+return (0);
+}
+else
+{
+_putchar ('-');
+return ('-1');
+}

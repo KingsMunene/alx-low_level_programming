d7e1e2fbc3af6a3de8d592467f42019e69b0644e
KingsMunene <munenekings2@gmail.com>
2022-03-23 22:02:06 +0000

    checks is upper

---
+++ 0x03-debugging/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_isupper - This function checks if a character is uppercase
+ *@c: the character being checked
+ *Return: Returns 1 if the character is uppercase and zero otherwise
+ */
+
+int _isupper(int c)
+{
+if ( c < 97)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

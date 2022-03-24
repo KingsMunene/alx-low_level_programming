36b3525e2d52de16dea640f9867119253208d36a
KingsMunene <munenekings2@gmail.com>
2022-03-24 09:22:15 +0000

    prints numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * print_numbers - prints numbers from 0 to 9
+ * @c: is the counter
  */
 
 void print_numbers(void)
@@ -12,4 +13,5 @@ while ( c <= 9)
 _putchar (c + '0');
 c++;
 }
+_putchar ('\n');
 }

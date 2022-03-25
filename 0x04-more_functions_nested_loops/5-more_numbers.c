94fad2e9f3fb0e0d7b0d062eb2a280f6c655074f
KingsMunene <munenekings2@gmail.com>
2022-03-25 08:29:50 +0000

    numbers in lines

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times numbers from 0 to 14
+ * more_numbers - prints 10 times numbers, from 0 to 14
  * followed by a new line 
  */
 
@@ -11,12 +11,16 @@ int c, d;
 
 for (c = 0; c < 10; c++)	
 {
+
 for (d = 0; d < 15; d++)
 {
+
 if (d >= 10)
 _putchar(d / 10 + '0');
 _putchar(d % 10 + '0');
+
 }
+
 _putchar('\n');
 
 }

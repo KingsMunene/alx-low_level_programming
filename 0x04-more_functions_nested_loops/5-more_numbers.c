f7ff67289278c43f666093132c0ee3d427cbaabd
KingsMunene <munenekings2@gmail.com>
2022-03-25 08:58:11 +0000

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,26 +2,20 @@
 
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
- * followed by a new line 
+ * followed by a new line
  */
-
 void more_numbers(void)
 {
-int c, d;
+  int i, j;
 
-for (c = 0; c < 10; c++)	
-{
-
-for (d = 0; d <=14; d++)
-{
-
-if (d >= 10)
-_putchar(d / 10 + '0');
-_putchar(d % 10 + '0');
-
-}
-
-_putchar('\n');
-
-}
+  for (i = 0; i < 10; i++)
+    {
+      for (j = 0; j < 15; j++)
+	{
+	  if (j >= 10)
+	    _putchar(j / 10 + '0');
+	  _putchar(j % 10 + '0');
+	}
+      _putchar('\n');
+    }
 }

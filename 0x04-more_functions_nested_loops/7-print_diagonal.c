de3f433a28c33c4be5a0453dcc44a43d386064f4
KingsMunene <munenekings2@gmail.com>
2022-03-25 10:37:04 +0000

    diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -20,16 +20,16 @@ for (i = 0; i < n; i++)
 {
 for (j = 0; j < n; j++)
 {
-	if (j == i)
-	{
-	_putchar('\\');
-	}
-	else if( j < i)
-	{
-	_putchar(' ');
-	}
+if (j == i)
+{
+_putchar('\\');
 }
-	_putchar ('\n');
+else if ( j < i)
+{
+_putchar(' ');
+}
+}
+_putchar ('\n');
 }
 }
 }

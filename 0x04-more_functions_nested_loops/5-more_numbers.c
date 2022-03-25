ec0f04e9eb80624a8ca5dc2224d8102e0b4c7140
KingsMunene <munenekings2@gmail.com>
2022-03-25 08:22:28 +0000

    numbers in lines

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,10 +7,9 @@
 
 void more_numbers(void)
 {
-int c = 0;
-int d;
+int c, d;
 
-while (c <= 10)
+for (c = 0; c < 10; c++)	
 {
 for (d = 0; d < 15; d++)
 {
@@ -18,7 +17,6 @@ if (d >= 10)
 _putchar(d / 10 + '0');
 _putchar(d % 10 + '0');
 }
-c++;
 _putchar('\n');
 
 }

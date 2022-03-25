87270d319a381cd57bfe605dfedfdc9a19de15ca
KingsMunene <munenekings2@gmail.com>
2022-03-25 09:13:42 +0000

    diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,17 +9,16 @@ void print_diagonal(int n)
 {
 if (n <= 0)
 {
-	_putchar('\n');
+_putchar('\n');
 }
 else
 {
-
-	int i = 0;
-	while(i < n)
-	{
-	_putchar(''\'');
-	_putchar('\n');
-	i++;
-	}
+int i = 0;
+while(i < n)
+{
+_putchar('\'');
+_putchar('\n');
+i++;
+}
 }
 }

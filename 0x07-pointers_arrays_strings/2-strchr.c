40b81ea10b1cd024f6ab544d069d53857e0b2c0e
KingsMunene <munenekings2@gmail.com>
2022-03-29 06:38:14 +0000

    pointers checker

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * _strchr - locate a character in a string
+ * *_strchr - locate a character in a string
  * @s: string to search
  * @c: char to find
  * Return: a pointer to the first occurrence of the character
- * c string
  */
 char *_strchr(char *s, char c)
 {

6f0ef63a3f19bd39970f77e2eddc009f1a1f9163
KingsMunene <munenekings2@gmail.com>
2022-03-29 05:09:19 +0000

    pointer

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,14 +5,15 @@
  * @c: char to find
  * Return: a pointer to the first occurrence of the character
  * c string
- * NULL if character is not found
  */
 char *_strchr(char *s, char c)
 {
 	unsigned int i;
+	
 	for (i = 0; *(s + 1) != '\0'; i++)
 		if(*(s + 1) == c)
 			return (s + i);
-	return (NULL);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
-

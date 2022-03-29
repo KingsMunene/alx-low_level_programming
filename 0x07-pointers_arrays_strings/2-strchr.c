5e96bb0381970903ac711565a185486b256b4c78
KingsMunene <munenekings2@gmail.com>
2022-03-29 06:41:33 +0000

    pointers checker

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,19 @@
 #include "main.h"
 /**
- * *_strchr - locate a character in a string
- * @s: string to search
- * @c: char to find
- * Return: a pointer to the first occurrence of the character
+ * * _strchr - Locates char and returns pointer
+ * @s: string
+ * @c: character
+ * Return: the pointer to the first occurence
  */
 char *_strchr(char *s, char c)
 {
 	unsigned int i;
-	
-	for (i = 0; *(s + i) != '\0'; i++)
-		if(*(s + i) == c)
-			return (s + i);
 
+	for (i = 0; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+		return (s + i);
 	if (*(s + i) == c)
 		return (s + i);
-
-	return ('\0');
+		return ('\0');
 }
 

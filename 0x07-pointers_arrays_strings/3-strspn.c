b907b1fc41d5a50736174ee4439e7990ff9fc938
KingsMunene <munenekings2@gmail.com>
2022-03-29 13:47:01 +0000

    numbers of bytes

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,27 +9,25 @@
  */
 unsigned int  _strspn(char *s, char *accept)
 {
-	int i, j, k, check;
+  int i, j, k, check;
 
-	k = 0;
+  k = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		check = 0;
-
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-			{
-				k++;
-				check = 1;
-			}
-		}
-		if (check == 0)
-		{
-			return (k);
-		}
+  for (i = 0; s[i] != '\0'; i++)
+  {
+    check = 0;
+    for (j = 0; accept[j] != '\0'; j++)
+      {
+	if (s[i] == accept[j])
+	  {
+	    k++;
+	    check = 1;
+	  }
 	}
-	return (0);
+    if (check == 0)
+      {
+	return (k);
+      }
+  }
+  return (0);
 }
-

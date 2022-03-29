c33e3526dfe6adc5c8461a03d31b0eeedde534c0
KingsMunene <munenekings2@gmail.com>
2022-03-29 08:41:00 +0000

    search the code

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,24 +12,26 @@ unsigned int  _strspn(char *s, char *accept)
 {
 
 	int i, j, k, check;
+
 	k = 0;
 
 	for (i = 0; s[i] != '\0'; j++)
 	{
 
 		check = 0;
+
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				k++;
-				flag = 1;
+				check = 1;
 
 			}
 		}
-		if (flag == 0)
+		if (check == 0)
 		{
-			return (f);
+			return (k);
 		}
 }
 

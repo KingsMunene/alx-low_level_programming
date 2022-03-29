9a092e99b084b8e021811945bda2d7cd7ce4e800
KingsMunene <munenekings2@gmail.com>
2022-03-29 16:33:58 +0000

    function finds the first occurrence of the substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,17 +1,15 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * *_strstr - locates a substring
  * @haystack: string to search in
  * @needle: substring to look for
- *
  * Return: pointer to the beginning of the located substring
  * or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
 {
-int i, j;  
+int i, j;
 for (i = 0; haystack[i] != '\0'; i++)
 {
 for (j = 0; needle[j] != '\0'; j++)

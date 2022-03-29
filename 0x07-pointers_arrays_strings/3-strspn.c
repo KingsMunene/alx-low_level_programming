cbf6b099b4c86393b7effa7b02b2c8b6a9357394
KingsMunene <munenekings2@gmail.com>
2022-03-29 09:36:46 +0000

    search the code

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * *_strspn - gets the length of a prefix substing
+ * _strspn - gets the length of a prefix substing
  * @s: string to evaluate
  * @accept: string containing the list of characters to match in s
+ * 
  * Return: the numbers of bytes in the initial segment of s which consist only of byte from accepts
  */
 
@@ -31,5 +32,5 @@ unsigned int  _strspn(char *s, char *accept)
 		}
 	}
 	return  (0);
-}	
+}
 

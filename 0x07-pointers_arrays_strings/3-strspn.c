7818aa2a068e4b53b15c255423cd6912db8cc341
KingsMunene <munenekings2@gmail.com>
2022-03-29 16:14:45 +0000

    string prefix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,25 +9,23 @@
  */
 unsigned int  _strspn(char *s, char *accept)
 {
-  int i, j, k, check;
-
-  k = 0;
-
-  for (i = 0; s[i] != '\0'; i++)
-  {
-    check = 0;
-    for (j = 0; accept[j] != '\0'; j++)
-      {
-	if (s[i] == accept[j])
-	  {
-	    k++;
-	    check = 1;
-	  }
-	}
-    if (check == 0)
-      {
-	return (k);
-      }
-  }
-  return (0);
+int i, j, k, check;
+k = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+check = 0;
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+k++;
+check = 1;
+}
+}
+if (check == 0)
+{
+return (k);
+}
+}
+return (0);
 }

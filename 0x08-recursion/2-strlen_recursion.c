7acadf039d987557e9a755ca1ebfcb0ad37fd3d3
KingsMunene <munenekings2@gmail.com>
2022-03-30 19:40:35 +0000

    length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - prints the length of a string
+ * @s: string to check
+ * Return: returns 0
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return 0;
+}
+return (s + _strlen_recursion(s++));
+}
+

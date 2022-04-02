63d683bc8a86269ea46c6acc87152e417cb9c662
KingsMunene <munenekings2@gmail.com>
2022-04-02 18:38:01 +0000

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,19 +5,20 @@
  * main - prints the minimum number of coins given change.
  * @argc: argument count
  * @argv: argument array
+ * 
  * Return: 1 or 0
  */
 
 int main(int argc, char *argv[])
 {
-int coins = 0;
-int change = atoi(argv[1]);
-
-if (argc > 2)
+if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
+int coins = 0;
+int change = atoi(argv[1]);
+
 
 if (argv[1] < 0)
 {

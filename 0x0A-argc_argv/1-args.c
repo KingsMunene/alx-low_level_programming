d1906a945933296c6e72bcddfc0904da78cbc14f
KingsMunene <munenekings2@gmail.com>
2022-04-02 11:15:26 +0000

    print the number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - gets the name of the program
 * @agrc: number of arguments
@@ -12,3 +13,4 @@ int main(int argc, char *argv[] __attribute__((unused)))
 printf("%i\n", argc - 1);
 return (0);
 }
+

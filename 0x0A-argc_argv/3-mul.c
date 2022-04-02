25d02039e6039ccf56ec631005422740e6cc5955
KingsMunene <munenekings2@gmail.com>
2022-04-02 12:59:54 +0000

    product of two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,26 +4,24 @@
 /**
  * main - multiplies two numbers and prints results
  * @argc: Argument count
- * @argv: Argument array 
+ * @argv: Argument array
  * Return: 0 if success
- * Return: 1 if no arguments
  */
 
 int main(int argc, char *argv[])
 {
-  int a, b, ans;
+int a, b, ans;
+ 
+if (argc > 1)
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+ans = (a * b);
   
-  if (argc > 1)
-    {
-      a = atoi(argv[1]);
-      b = atoi(argv[2]);
-      ans = (a * b);
-  
-      printf("%d\n", ans);    
-      return (0);
+printf("%d\n", ans);
+return (0);
 }
 
-  printf("Error\n");
-
-  return (1);
+printf("Error\n");
+return (1);
 }

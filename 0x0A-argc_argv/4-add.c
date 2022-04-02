2fa39e3d8de4192a819bb53354db81398aeafc94
KingsMunene <munenekings2@gmail.com>
2022-04-02 16:27:45 +0000

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,14 +5,13 @@
  * main - adds two positive number
  * @argc: number of arguments
  * @argv: array of arguents
- *
  * Return: 0 (Success), or 1 (Success)
  */
 int main(int argc, char *argv[])
 {
 int sum, num, i, j, k;
 sum = 0;
- 
+
 for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j] != '\0'; j++)

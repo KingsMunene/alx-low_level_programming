c83bbec4a0eb1189cd422c92269fc9fd0a97b072
KingsMunene <munenekings2@gmail.com>
2022-04-02 16:14:21 +0000

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,37 +2,39 @@
 #include <stdlib.h>
 
 /**
- * main - adds positive numbers
- * @argc: argument count
- * @argv: arguments array
- * Return: 0 (success), 1 (fail)
+ * main - adds two positive number
+ * @argc: number of arguments
+ * @argv: array of arguents
+ *
+ * Return: 0 (Success), or 1 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-int i,j,k;
-int sum = 0;
+  int sum, num, i, j, k;
 
-if (argc <= 0)
-{
-printf("%d\n", 0);
-}
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0'; j++)
-{
-if (argv[i][j] < '0' || argv[i][j] > '9')
-{
-printf("Error\n");
-return (1);
-}}}
-for (k = 0; k < argc; k++)
-{
-if (*argv[k] != '\0')
-{
-sum+=atoi(argv[k]);
-}}
-printf("%d\n", sum);
-return (0);
-}
+  sum = 0;
 
+  for (i = 1; i < argc; i++)
+    {
+      for (j = 0; argv[i][j] != '\0'; j++)
+	{
+	  if (argv[i][j] > '9' || argv[i][j] < '0')
+	    {
+	      puts("Error");
+	      return (1);
+	    }
+	}
+    }
+
+  for (k = 1; k < argc; k++)
+    {
+      num = atoi(argv[k]);
+      if (num >= 0)
+	{
+	  sum += num;
+	}
+    }
+
+  printf("%d\n", sum);
+  return (0);
+}

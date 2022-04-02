e30f0eb2cecadba8ee815afec15afaf13edcbd55
KingsMunene <munenekings2@gmail.com>
2022-04-02 16:23:29 +0000

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,7 +12,11 @@ int main(int argc, char *argv[])
 {
 int sum, num, i, j, k;
 sum = 0;
-
+if (argc < 0)
+{
+printf("%d\n", 0);
+}
+ 
 for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j] != '\0'; j++)

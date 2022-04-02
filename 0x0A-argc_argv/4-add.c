27b208cececa3b3fb0c3df2d8356b6b6635a44c0
KingsMunene <munenekings2@gmail.com>
2022-04-02 15:10:57 +0000

    cahnges

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,30 +12,30 @@
  */
 int isInt(char *s)
 {
-  int i = 0;
+int i = 0;
 
-  while(s[i] != '\0')
-    {
-      if(s[i] < '0' || s[i] > '9')
-	return (1);
-      i++;
-    }
-  return (0);
+while(s[i] != '\0')
+{
+if(s[i] < '0' || s[i] > '9')
+return (1);
+i++;
+}
+return (0);
 }
 
 int main(int argc, char *argv[])
 {
-  int sum = 0;
+int sum = 0;
 
-  while(--argc)
-    {
-      if(isInt(argv[argc]))
-	{
-	  printf("Error\n");
-	  return (1);
-	}
-      sum += atoi(argv[argc]);
-    }
-  printf("%i\n", sum);
-  return (0);
+while(--argc)
+{
+if(isInt(argv[argc]))
+{
+printf("Error\n");
+return (1);
+}
+sum += atoi(argv[argc]);
+}
+printf("%i\n", sum);
+return (0);
 }

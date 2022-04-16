6330cd8336e326c986a9240feae8b0a75de8fc2e
kariuki <munenekings2@gmail.com>
2022-04-16 13:16:14 +0300

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+(15 sloc)  287 Bytes
+  
+#include "function_pointers.h"
+
+/**
+ * print_name - print the name of input string with input function
+ * @name: input name string
+ * @f: pointer to function to print string
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		(*f)(name);
+	}
+}

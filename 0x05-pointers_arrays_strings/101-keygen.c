ab01c5327a73793d8be1cf339baefb8a59760702
KingsMunene <munenekings2@gmail.com>
2022-05-07 10:55:44 +0000

    random password generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	int i = 0, j = 0, k = 0;
+
+	int numbers[10];
+
+	char password[10];
+
+	srand(time(NULL));
+
+/*Generate random numbers that will be looped through alphabests */
+	while (j < 10)
+	{
+		numbers[j] = (rand() % 10);
+		j++;
+	}
+/*alphabets*/
+	char alphabets[] = {'A','B','C','D','E','F','G','H','I','J','K','L'};
+/*Loop through the alphabets getting the alphabet represented by
+  a random number */
+		while (i < 10)
+		{
+			password[i] = alphabets[numbers[i]];
+
+
+			i++;
+		}
+
+		printf("\n");
+}
